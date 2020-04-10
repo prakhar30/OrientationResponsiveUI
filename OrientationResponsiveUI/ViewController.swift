9549d07f863113488af63c2e40154dcd85246a5e
Prakhar Tripathi <prakhar.tripathi@theredpandas.com>
2020-04-10 20:02:33 +0530

    added barebones project

---
+++ OrientationResponsiveUI/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  OrientationResponsiveUI
+//
+//  Created by Prakhar Tripathi on 10/04/20.
+//  Copyright © 2020 Prakhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        view.backgroundColor = UIColor.green
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Orientation Responsive UI
+
+
+A sample project to demonstrate how we can have different UI based on the orientation of the device, ie a different layout when the device is in portrait and a different one when in landscape.
