bf5334bae11acb719b82e896622977f1630000ea
Prakhar Tripathi <prakhar.tripathi@theredpandas.com>
2020-04-10 22:33:32 +0530

    added code to detect orientation

--- OrientationResponsiveUI/ViewController.swift
+++ OrientationResponsiveUI/ViewController.swift
@@ -13,10 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(self.orientationChanged), name: UIApplication.didChangeStatusBarOrientationNotification, object: nil)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         view.backgroundColor = UIColor.green
+    }
+    
+    @objc func orientationChanged(notification: NSNotification) {
+        let deviceOrientation = UIApplication.shared.statusBarOrientation
+
+        switch deviceOrientation {
+        case .portrait:
+            fallthrough
+        case .portraitUpsideDown:
+            print("Portrait")
+
+        case .landscapeLeft:
+            fallthrough
+        case .landscapeRight:
+            print("landscape")
+        case .unknown:
+            print("unknown orientation")
+        @unknown default:
+            print("unknown case in orientation change. Note")
+        }
     }
 }
